e6d62149253ff35df3ffee150999ae376d51eb9b
Florian Rudin <flaudre@gmail.com>
2017-12-13 17:04:32 +0800

    fixed bug if config.db is undefined

--- mongodb/connect.js
+++ mongodb/connect.js
@@ -4,7 +4,7 @@ exports.connect = function (config, env) {
 
   console.log("Connecting to db: " + config.DB_NAME);
 
-  if (config.db.user && config.db.pass) {
+  if (config.db && config.db.user && config.db.pass) {
     mongoose.connect('mongodb://localhost:27017/' + config.DB_NAME, {
         user: config.db.user,
         pass: config.db.pass
