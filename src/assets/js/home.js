46c93fc461f1feee873be6fac787539da8424a60
Colin Shaw <xkidshaw@gmail.com>
2018-06-09 23:45:21 +0800

    home page js effect

--- src/assets/js/home.js
+++ src/assets/js/home.js
@@ -3,7 +3,9 @@ var app = require('./globals');
 
 app.then(function(){
 
-	if ( $(window).width() > 1080) {      
+	var windowWidth = $(window).width();
+
+	if ( windowWidth > 1080) {      
 		$('.menuBar .menu a').click(function(e) {
 			e.preventDefault();
 			$('.menuBar').addClass('goToTop');
@@ -25,6 +27,49 @@ app.then(function(){
 	}
 
 
+	function setImagePlacement(){
+		var width = $('.movingImage .active img').width(),
+		height = $('.movingImage .active img').height();
+		$('.movingImage').css('width', windowWidth + width + windowWidth);
+		$('.movingImage').css('margin-top', -height/2);
+		$('.movingImage img').css('margin-right', windowWidth - width );
+	}
+
+	setImagePlacement();
+
+
+	$('.menuBar .menu a').mouseenter(function(){
+		var url = $(this).attr('href').split("/"),
+		last_part = url[url.length-1];
+
+		$('.movingImage').attr('id', last_part);
+		$(".movingImage div").css('opacity',0);
+		setTimeout(function() {
+			$('.movingImage div').removeClass('active');
+			$(".movingImage div." + last_part + "").addClass('active');
+			setTimeout(function() {
+				$(".movingImage div." + last_part + "").css('opacity',1);
+				setImagePlacement();
+			}, 300);
+		}, 300);
+	});
+
+
+	$('.menuBar .menu a').mouseleave(function(){
+		$(".movingImage div").css('opacity',0);
+		setTimeout(function() {
+			$('.movingImage').attr('id', '');
+			$('.movingImage div').removeClass('active');
+			$('.movingImage div.default').addClass('active');
+			setTimeout(function() {
+				$('.movingImage div.default').css('opacity',1);
+				setImagePlacement();
+			}, 300);
+		}, 300);
+	});
+
+
+	
 
 	require(['../vendor/jcarousel/jcarousel.basic.css']);
 
