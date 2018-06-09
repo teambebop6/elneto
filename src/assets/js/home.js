697f013fd454b225d8282283e740ed491756aa79
Colin Shaw <xkidshaw@gmail.com>
2018-06-09 21:28:36 +0800

    responsive for menu and home page, smooth transition

--- src/assets/js/home.js
+++ src/assets/js/home.js
@@ -3,6 +3,29 @@ var app = require('./globals');
 
 app.then(function(){
 
+	if ( $(window).width() > 1080) {      
+		$('.menuBar .menu a').click(function(e) {
+			e.preventDefault();
+			$('.menuBar').addClass('goToTop');
+			var $a = $(this).addClass('active');
+			setTimeout(function() {
+				window.location.assign($a.attr('href'));
+			}, 500);
+		});
+	} 
+	else {
+		$('.menuBar .menu a').click(function(e) {
+			e.preventDefault();
+			var $a = $(this).addClass('active');
+			$('.mask').css('opacity','1');
+			setTimeout(function() {
+				window.location.assign($a.attr('href'));
+			}, 500);
+		});
+	}
+
+
+
 	require(['../vendor/jcarousel/jcarousel.basic.css']);
 
 	require(['../vendor/jcarousel/jquery.jcarousel.min', '../vendor/jcarousel/jcarousel.basic'], function(){
