7ca3a0fe0ed5a684292053fae9f03c2a7f78d97f
henryhuang <h1886@outlook.com>
2019-09-17 00:28:06 +0800

    add log

--- src/tasks/create_user.js
+++ src/tasks/create_user.js
@@ -3,14 +3,16 @@ var User = require('../mongodb/user_model')
 var Counter = require('../mongodb/counter_model')
 
 var env = process.env.NODE_ENV || 'development';
+console.log(`Env is ${env}`);
 
 var config = require('../config')(env);
+console.log(JSON.stringify(config));
 
 var db = require('../mongodb/connect');
 db.connect(config, env);
 
 if(process.argv[2] && process.argv[3]){
-	
+
 	console.log("Password is: " + process.argv[3]);
 	var user = new User({
 		_id : process.argv[2],
